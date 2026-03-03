5ee1485ed4fd0a5a0c5a65f043637f34435b2f50
KhushiDubey2006 <chmcs2026.khushi.dubey@gmail.com>
2026-03-03 13:30:07 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+     baseURL:"https://course-catalog-system-8wfy.onrender.com"
 });
 
 export default api;
